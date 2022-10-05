068d2f0794c84a7649e0b219ae4415019af1ed27
KenJi <89127152+Khenteb@users.noreply.github.com>
2022-10-05 21:44:10 +0600

    ClassWork
    Check the similarity between the sentence base on the word similarity

--- flask-ml/pre_trained_model/utils.py
+++ flask-ml/pre_trained_model/utils.py
@@ -11,7 +11,7 @@ def similarity_value(s1, s2):
   
     if sim.numpy() <= -0.6:
         msg = f"Cosine Similarity is {sim.numpy()}, this indicates high similarity"
-    elif sim.numpy() >= 0.6:
+    elif sim.numpy() >= -0.59:
         msg = f"Cosine Similarity is {sim.numpy()}, this indicates high dissimilarity"
     else:
         msg = f"Cosine Similarity is {sim.numpy()}, can't decide much with this value"
